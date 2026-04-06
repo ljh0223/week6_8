31cd839be0fe88980486b505eb78ae1fa632c246
이재혁 <cncnvmvm@naver.com>
2026-04-06 20:20:40 +0900

    스택,큐 6번 구현완료

--- Data-Structures/Stack_and_Queue/Q6_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q6_C_SQ.c
@@ -111,7 +111,12 @@ int main()
 
 void removeUntil(Stack *s, int value)
 {
-/* add your code here */
+	if (s==NULL){
+		return;
+	}
+	while (!isEmptyStack(s) && peek(s)!=value){
+		pop(s);
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////
